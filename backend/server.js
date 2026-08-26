ba7ddc7a9868a7ee6eb7ca0b2e435b9ede8e8855
L.G.R.J.Lindapitiya <randilujayani31@gmail.com>
2026-08-26 22:33:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const apiRoutes = require('./routes');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Use API Routes
+app.use('/api', apiRoutes);
 
 // Test Route
 app.get('/', (req, res) => {
@@ -14,5 +18,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
